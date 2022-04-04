812e1a2abb17fb8c056d088efcc5445c9d2f4988
CMOS <102813006+sentricts@users.noreply.github.com>
2022-04-04 14:45:03 -0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function opentab(evt, currentTab) {
+  var i, tabcontent, tablinks;
+
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  console.log("done");
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(currentTab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
